6ed9a9dbc605d4a21f15f98176cb7eb079b6890d
Jose 2.0 <jmorbar748.g.educaand.es>
2026-04-08 13:05:21 +0200

    feat: Agregamos Clientes

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Clientes.java
@@ -0,0 +1,72 @@
+package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
+
+import org.iesalandalus.programacion.tallermecanico.modelo.TallerMecanicoExcepcion;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Clientes {
+    private final List<Cliente> coleccionClientes;
+
+    public Clientes() {
+        coleccionClientes = new ArrayList<>();
+    }
+
+    public List<Cliente> get() {
+        return new ArrayList<>(coleccionClientes);
+    }
+
+    public void insertar(Cliente cliente) throws TallerMecanicoExcepcion {
+        if (cliente == null) {
+            throw new NullPointerException("No se puede insertar un cliente nulo.");
+        }
+        if (coleccionClientes.contains(cliente)) {
+            throw new TallerMecanicoExcepcion("Ya existe un cliente con ese DNI.");
+        }
+        coleccionClientes.add(cliente);
+    }
+
+    public Cliente modificar(Cliente cliente, String nombre, String telefono) throws TallerMecanicoExcepcion {
+        if (cliente == null) {
+            throw new NullPointerException("No se puede modificar un cliente nulo.");
+        }
+        Cliente clienteExistente = buscar(cliente);
+        if (clienteExistente == null) {
+            throw new TallerMecanicoExcepcion("No existe ningún cliente con ese DNI.");
+        }
+        if (nombre != null && !nombre.isBlank()) {
+            clienteExistente.setNombre(nombre);
+
+        }
+        if (telefono != null && !telefono.isBlank()) {
+            clienteExistente.setTelefono(telefono);
+        }
+        return new Cliente(cliente);
+    }
+
+    public Cliente buscar(Cliente cliente) {
+        if (cliente == null) {
+            throw new NullPointerException("No se puede buscar un cliente nulo.");
+        }
+        for (Cliente c : coleccionClientes) {
+            if (c.equals(cliente)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+
+    public void borrar(Cliente cliente) throws TallerMecanicoExcepcion {
+        if (cliente == null) {
+            throw new NullPointerException("No se puede borrar un cliente nulo.");
+        }
+        Cliente clienteExistente = buscar(cliente);
+        if (clienteExistente == null) {
+            throw new TallerMecanicoExcepcion("No existe ningún cliente con ese DNI.");
+        }
+        coleccionClientes.remove(clienteExistente);
+    }
+}
+
